b8556a44afa58844a062e35ff826b974ab1e7d4a
Jack <jack.cairns22@gmail.com>
2024-12-29 21:40:10 +1000

    Engine should now be functional. Game can be basically played using the text printed version

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout,
                              QLabel, QPushButton, QMainWindow,
                              QMessageBox, QFrame, QHBoxLayout)
 
+ROWS: int = 20
+COLUMNS: int = 40
+
 
 class ViewController(QMainWindow):
     def __init__(self) -> None:
@@ -17,10 +20,10 @@ class ViewController(QMainWindow):
 
 class Engine(QObject):
     # Signals
-    def __init__(self, board_height: int, board_width: int) -> None:
+    def __init__(self, board_rows: int, board_columns: int) -> None:
         super().__init__()
-        self.board_width: int = board_width
-        self.board_height: int = board_height
+        self.board_columns: int = board_columns
+        self.board_rows: int = board_rows
         self.snake: list[tuple[int, int]] | None = None
         self.current_dir: tuple[int, int] | None = None
         self.food: tuple[int, int] | None = None
@@ -31,21 +34,56 @@ class Engine(QObject):
     def generate_start(self) -> None:
         # random.seed(None)
         self.current_dir = random.choice(self._directions)
-        self.snake = [(random.choice(range(1, self.board_height - 1)), random.choice(range(1, self.board_width - 1)))]
+        # Snake starts 3 long
+        self.snake = [(random.choice(range(1, self.board_rows - 2)), random.choice(range(1, self.board_columns - 2)))]
+        self.snake.append((self.snake[0][0] + self.current_dir[0] * -1, self.snake[0][1] + self.current_dir[1] * -1))
+        self.snake.append((self.snake[1][0] + self.current_dir[0] * -1, self.snake[1][1] + self.current_dir[1] * -1))
         self.food = self.generate_food()
 
     def generate_food(self) -> tuple[int, int]:
-        all_positions: set[tuple[int, int]] = {(x, y) for x in range(self.board_height)
-                                               for y in range(self.board_width)}
+        all_positions: set[tuple[int, int]] = {(x, y) for x in range(self.board_rows)
+                                               for y in range(self.board_columns)}
         available_positions: set[tuple[int, int]] = all_positions - set(self.snake)
         if not available_positions:
             return -1, -1
 
         return random.choice(list(available_positions))
 
+    def change_direction(self, direction: tuple[int, int]) -> bool:
+        if direction not in self._directions:
+            return False
+
+        self.current_dir = direction
+        return True
+
+    def move_snake(self) -> None:
+        new_head: tuple[int, int] = (self.snake[0][0] + self.current_dir[0], self.snake[0][1] + self.current_dir[1])
+        self.snake.insert(0, new_head)
+        if new_head != self.food:
+            self.snake.pop()
+            return
+
+        # Food has been eaten -> generate new food
+        self.food = self.generate_food()
+
+    def check_collision(self) -> bool:
+        destination: tuple[int, int] = self.snake[0][0] + self.current_dir[0], self.snake[0][1] + self.current_dir[1]
+        for position in self.snake:
+            if position == destination:
+                return True
+
+        row, col = destination
+        if 0 > row or row >= ROWS or 0 > col or col >= COLUMNS:
+            return True
+
+        return False
+
+    def is_game_over(self) -> bool:
+        return self.food == (-1, -1)
+
     def print_game_board(self) -> None:
         # Create an empty board
-        board = [['.' for _ in range(self.board_height)] for _ in range(self.board_width)]
+        board = [['.' for _ in range(self.board_columns)] for _ in range(self.board_rows)]
 
         # Place the food on the board
         food_x, food_y = self.food
@@ -61,16 +99,39 @@ class Engine(QObject):
         print()  # Extra newline for spacing
 
 
-def print_random() -> None:
-    number = int(input("Enter a number: "))
-    for _ in range(number):
-        print(random.choice(range(number)))
+def play_game() -> None:
+    engine = Engine(ROWS, COLUMNS)
+    engine.print_game_board()
+    engine.move_snake()
+    engine.print_game_board()
+    while True:
+        move = input("Move snake: up/down/left/right: ").lower()
+        if move == 'up':
+            direction = (-1, 0)
+        elif move == 'down':
+            direction = (1, 0)
+        elif move == 'left':
+            direction = (0, -1)
+        elif move == 'right':
+            direction = (0, 1)
+        else:
+            print('Invalid move')
+            continue
+
+        engine.change_direction(direction)
+        if engine.check_collision() or engine.is_game_over():
+            print("Game over!")
+            response = input("Would you like to play again? (y/n): ").lower()
+            if response == 'y':
+                play_game()
+            else:
+                print("Bye")
+                return
+
+        engine.move_snake()
+        engine.print_game_board()
 
 
 if __name__ == '__main__':
-    # print_random()
-    engine = Engine(20, 20)
-    for _ in range(5):
-        engine.print_game_board()
-        print(engine.current_dir)
-        engine.generate_start()
+    play_game()
+
